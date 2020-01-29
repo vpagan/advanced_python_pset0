e1e01db995b71d1009c6602ff4b4aeee4e443d94
vap1 <vicentepagan@ymail.com>
2020-01-28 20:32:27 -0500

    Optimized fibonacci (1)

--- fibonacci.py
+++ fibonacci.py
@@ -11,8 +11,15 @@ def last_8(some_int):
     raise NotImplementedError()
 
 
-def optimized_fibonacci(f):
-    raise NotImplementedError()
+def optimized_fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    a, b = 0, 1
+    for i in range(2, n + 1):
+        a, b = b, a + b
+    return b
 
 
 class SummableSequence(object):
